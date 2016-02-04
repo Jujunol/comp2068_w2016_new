4baffa22dd2a224dc49c2fbaa5954de6dc7c4f90
Jujunol <jujunol@outlook.com>
2016-02-04 20:29:14 +0000

    Setup new format with Jade

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 /// <reference path="typings/_reference.d.ts" />
 var express = require('express');
-var path = require('path');
+// import path = require('path');
 var app = express();
 var port = process.env.port || 8080;
 app.set('views', './views');
 app.set('view engine', 'jade');
-app.use('/scripts/lib/', express.static(__dirname + '/scripts/lib'));
+app.use('/lib', express.static(__dirname + '/lib'));
+app.use('/lib', express.static(__dirname + '/scripts'));
 app.get('/', function (req, res) {
-    res.render('index');
-}).get('/info', function (req, res) {
-    res.sendfile(path.join(__dirname, "public", "info.html"));
+    res.render('index', { "title": "COMP2068 Class " });
 });
 app.listen(port, function () {
     console.log('App server started on port: ' + port);
--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 /// <reference path="typings/_reference.d.ts" />
 
 import express = require('express');
-import path = require('path');
+// import path = require('path');
 
 var app:express.Express = express();
 var port:number = process.env.port || 8080;
@@ -9,12 +9,11 @@ var port:number = process.env.port || 8080;
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.use('/scripts/lib/', express.static(__dirname + '/scripts/lib'));
+app.use('/lib', express.static(__dirname + '/lib'));
+app.use('/lib', express.static(__dirname + '/scripts'));
 
 app.get('/', function(req:express.Request, res:express.Response) {
-    res.render('index');
-}).get('/info', function(req:express.Request, res:express.Response) {
-    res.sendfile(path.join(__dirname, "public", "info.html"));
+    res.render('index', { "title": "COMP2068 Class " });
 });
 
 app.listen(port, function() {
